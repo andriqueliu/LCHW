cb1deb4e4c07dd85df25646fb31bd46de229b179
Andrique <andriqueliu@gmail.com>
2017-02-19 00:54:55 -0800

    Add ex12, begin breaking

---
+++ ex12/ex12.c
@@ -0,0 +1,29 @@
+// Note!!! 
+// plox[n] is the same as *(plox + n)
+
+// Note: sizeof returns size in BYTES
+
+#include <stdio.h>
+
+int main()
+{
+	// Note: the array DOES NOT have a width assigned to it...
+	// The way this works is... C notes the desired size you want, so it just
+	// does it for you
+	int areas[] = { 10, 12, 13, 14, 20 };
+	char name[] = "Zed";
+	// The full_name method and the name methods are identical, behaviorally
+	// char full_name[] = {
+
+	// }
+
+	printf("The size of an int: %ld\n", sizeof(int));
+	printf("The size of areas (int[]): %ld\n", sizeof(areas));
+	printf("The number of ints in areas: %ld\n",
+		    sizeof(areas) / sizeof(int));
+	printf("The first area is %d, the second %d\n", areas[0], areas[1]);
+
+	printf("name: %s\n", name);
+
+	
+}
