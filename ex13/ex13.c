766e265ce64854e51f82887184f7b9b7ccb70cfd
Andrique <andriqueliu@gmail.com>
2017-02-19 01:44:14 -0800

    Run tests

--- ex13/ex13.c
+++ ex13/ex13.c
@@ -1,3 +1,5 @@
+// Note: %s expects a char *
+
 #include <stdio.h>
 
 int main(int argc, char *argv[])
@@ -11,16 +13,37 @@ int main(int argc, char *argv[])
 	}
 
 	// Let's make our own array of strings
-	char *states = {
+	// ??? An array of strings... sth *plox[]
+	// This is making an array of four pointers to strings!
+	char *states[] = {
 		"California", "Oregon",
 		"Washington", "Texas"
 	};
 
-	int num_states = 4;
+	printf("%s\n", states[0]);
 
-	for (i = 0; i < num_states; i++) {
+	// This actually works!
+	// states[0] is the first char *
+	states[0] = "Hello";
+
+	// Note: Going OOB prints garbage
+	// int num_states = 4;
+	int num_states;
+	// Try changing num states in the for loop
+
+	for (i = 0, num_states = 4; i < num_states; i++) {
 		printf("state %d: %s\n", i, states[i]);
 	}
+
+	// This does work... 
+	argv[0] = "Hi";
+	printf("%s\n", argv[0]);
+
+	// This also works???
+	if (argc > 1) {
+		states[0] = argv[1];
+	}
+	printf("%s\n", states[0]);
 
 	return 0;
 }
