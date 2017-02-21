e9579ca31c4a8ab256313f373477673b8b7833b5
Andrique <andriqueliu@gmail.com>
2017-02-20 20:07:08 -0800

    Add comments about stacks and heaps

---
+++ ex17/ex17.c
@@ -0,0 +1,15 @@
+/*
+Stack vs. Heap
+
+So for practical purposes, malloc == heap, else == stack.
+This isn't a perfect representation, but it'll do for now.
+
+If you need something to stick around for a long time, a big chunk of data,
+etc., go heap.
+Else, if you need something around only for a little bit and need it really fast,
+go stack. 
+
+Lots more details...
+*/
+
+
